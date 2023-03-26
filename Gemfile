72fa536963c4ec15d721a236f87807d5dc506ce6
Collins Kiragu <collins.kiragu@student.moringaschool.com>
2023-03-26 15:32:09 +0300

    Done

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'sqlite3', '~> 1.4'
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
 # Use Active Model has_secure_password
-gem 'bcrypt', '~> 3.1.7'
+# gem 'bcrypt', '~> 3.1.7'
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
@@ -27,10 +27,6 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-group :development do
-  gem 'foreman', '~> 0.87'
-end
-
 group :development, :test do
   gem 'rspec-rails', '~> 5.0.0'
 end
@@ -40,4 +36,5 @@ group :test do
   gem 'shoulda-matchers', '~> 4.0'
 end
 
-gem "active_model_serializers", "~> 0.10.12"
+gem 'faker', '~> 2.17'
+gem 'active_model_serializers'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,76 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+author1 = Author.create(
+  name: "Wilbert Green"
+)
+
+author1.create_profile(
+  email: "wgreen@kshlerin.co", 
+  username: "willygreen", 
+  bio: "Keytar wolf bushwick seitan mumblecore try-hard marfa.", 
+  avatar_url: "https://robohash.org/voluptasquisnihil.png?size=300x300&set=set1"
+)
+
+author2 = Author.create(
+  name: "Sonya Wolf "
+)
+
+author2.create_profile(
+  email: "swolf@keeling.name", 
+  username: "sonyaw", 
+  bio: "Park bespoke diy selfies leggings shabby chic kombucha taxidermy.", 
+  avatar_url: "https://robohash.org/eosdoloremest.png?size=300x300&set=set1"
+)
+
+author3 = Author.create(
+  name: "Millicent Rath"
+)
+
+author3.create_profile(
+  email: "millie@toy-hauck.org", 
+  username: "millie", 
+  bio: "Celiac meh scenester. Keytar hammock phlogiston. Before they sold out blue bottle pug whatever.", 
+  avatar_url: "https://robohash.org/reiciendisexplicaboaliquid.png?size=300x300&set=set1"
+)
+
+post1 = author1.posts.create(
+  title: "A Monstrous Regiment of Women", 
+  content: "Stumptown photo booth chartreuse next level biodiesel. Park narwhal cronut hammock helvetica."
+)
+
+post1.tags.create(
+  name: "repellat"
+)
+
+post1.tags.create(
+  name: "qui"
+)
+
+post2 = author1.posts.create(
+  title: "Clouds of Witness", 
+  content: "Selfies pickled crucifix offal. Celiac art party godard narwhal retro freegan selvage gastropub. Leggings brunch biodiesel gastropub schlitz cold-pressed truffaut. Master hoodie chia banh mi offal knausgaard yolo. Authentic selfies twee pabst tumblr. Freegan keffiyeh cliche gastropub. Yr vinyl cliche synth freegan forage. Biodiesel food truck fixie lo-fi 8-bit drinking neutra."
+)
+
+post2.tags.create(
+  name: "qui"
+)
+
+post2.tags.create(
+  name: "facere"
+)
+
+post3 = author2.posts.create(
+  title: "Antic Hay", 
+  content: "Typewriter neutra five dollar toast plaid. Polaroid tumblr put a bird on it."
+)
+
+post3.tags.create(
+  name: "occaecati"
+)
+
+post4 = author3.posts.create(
+  title: "Look Homeward, Dylan", 
+  content: "Selfies pickled crucifix offal. Celiac art party godard narwhal retro freegan selvage gastropub. Leggings brunch biodiesel gastropub schlitz cold-pressed truffaut. Master hoodie chia banh mi offal knausgaard yolo. Authentic selfies twee pabst tumblr. Freegan keffiyeh cliche gastropub. Yr vinyl cliche synth freegan forage. Biodiesel food truck fixie lo-fi 8-bit drinking neutra."
+)
+
+post4.tags.create(
+  name: "facere"
+)
